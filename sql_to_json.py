e4b8632a50e3fef7485209dd7a61631363e47cd2
dnagis <vincent.achard@gmail.com>
2019-12-02 14:32:32 +0100

    table net de la bdd

--- sql_to_json.py
+++ sql_to_json.py
@@ -2,6 +2,7 @@
 
 '''
 creer un json gobable leafletjs, a partir d'une bdd sqlite
+CREATE TABLE net (ID INTEGER PRIMARY KEY AUTOINCREMENT, ASYNCID INTEGER NOT NULL, STARTTIME INTEGER NOT NULL, ENDTIME INTEGER NOT NULL, HTTPREPLY INTEGER NOT NULL, NLOCS INTEGER NOT NULL, LAT REAL NOT NULL, LONG REAL NOT NULL);
 '''
  
 import sqlite3
@@ -11,7 +12,7 @@ from datetime import datetime
 db_file = '/root/loc.db'
 conn = sqlite3.connect(db_file)
 cur = conn.cursor()
-cur.execute("SELECT fixtime, lat, long, alt, acc FROM loc")
+cur.execute("SELECT asyncid, starttime, endtime, httpreply, nlocs, lat, long FROM net")
 rows = cur.fetchall()
 
 
@@ -20,10 +21,10 @@ locs_array = []
 length = len(rows) 
 
 for i in range(length-1):
-	featureDict = {"lat": rows[i][1], "long": rows[i][2]}
+	featureDict = {"asyncid":rows[i][0],"starttime":rows[i][1],"endtime":rows[i][2],"httpreply":rows[i][3],"nlocs":rows[i][4],"lat": rows[i][5], "long": rows[i][6]}
 	locs_array.append(featureDict)
 
 
-print "locs_array =",locs_array
+print "net_array =",locs_array
 
 
